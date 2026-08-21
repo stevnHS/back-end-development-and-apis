9dee05e8ab7ed7829a4e87b484ac204d429fcb3e
Stevn <117241570+stevnHS@users.noreply.github.com>
2026-08-21 02:44:58 +0000

    finish node.js project2

---
+++ build-a-personal-profile-app/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res) => res.send("Welcome to Camper Bot's homepage!"))
+app.get('/hobbies',(req,res) => res.send("I cycle, go boating, and play guitar."))
+app.get('/skills',(req,res) => res.send("JavaScript, Node.js, and Express.js!"))
+
+const profile = {
+    name:  `Camper Bot`,
+    hobbies: ['cycling', 'boating', 'guitar'],
+    skills: ['JavaScript', 'Node.js', 'Express.js']
+}
+app.get('/api/profile',(req,res) => res.json(profile))
+
+app.listen(3000);
