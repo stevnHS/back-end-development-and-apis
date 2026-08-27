8c46bebea95c9de9103a5a794b358624fd1f2cc7
Stevn <117241570+stevnHS@users.noreply.github.com>
2026-08-27 08:05:11 +0000

    workshop:websocket

--- build-a-chat-app/server.js
+++ build-a-chat-app/server.js
@@ -1,5 +1,37 @@
 import http from 'http';
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { WebSocketServer } from 'ws';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = 3001;
+
+const server = http.createServer((req, res) => {
+    // 取得 HTML 檔案的路徑（假設為同目錄下的 index.html）
+    const filePath = path.join(__dirname, './public/index.html');
+
+    // 讀取檔案
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end('500 Internal Server Error');
+            return;
+        }
+
+        // 回傳 HTML 內容
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(data);
+    });
+});
+
+const wss = new WebSocketServer({ server })
+
+// wss.on('connection', (socket, req) => {
+
+// })
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
